47cabe779731aaa0a6cfab55739d1d177574690b
gabrielab22 <gabriela.bilanzic@gmail.com>
2024-06-03 21:52:59 +0200

    add filter in list of product

--- arthub/src/components/Products/ProductGrid.tsx
+++ arthub/src/components/Products/ProductGrid.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ProductCard from "./ProductCard";
-import { SimpleGrid } from "@chakra-ui/react";
-import { Product } from "../../types";
+import { SimpleGrid, Select } from "@chakra-ui/react";
+import { Product, Variant } from "../../types";
 
 const ProductGrid: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [selectedType, setSelectedType] = useState<Variant | "all">("all");
 
   useEffect(() => {
     async function fetchData() {
@@ -19,12 +20,31 @@ const ProductGrid: React.FC = () => {
     fetchData();
   }, []);
 
+  const filteredProducts =
+    selectedType === "all"
+      ? products
+      : products.filter((product) => product.variant === selectedType);
+
+  const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedType(event.target.value as Variant | "all");
+  };
+
   return (
-    <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing="4">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </SimpleGrid>
+    <div>
+      <Select value={selectedType} onChange={handleTypeChange}>
+        <option value="all">All Types</option>
+        {Object.values(Variant).map((type) => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
+      </Select>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing="4">
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </SimpleGrid>
+    </div>
   );
 };
 
